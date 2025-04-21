7a2302e52ad5e3a9f0d7d3066bfedb1e667ddad1
bics <bicsi94@gmail.com>
2025-04-21 16:09:41 +0100

    MS-More testing

--- assets/scripts/gamemanager.js
+++ assets/scripts/gamemanager.js
@@ -134,7 +134,7 @@ function revealTile(element) {
 }
 
 /** Cycle through all neighbour tiles, and reveal recursively
- * Current limit with 2 mines is 62*62
+ * Current limit with 2 mines is 62*62 with occasional overflow
  */
 function revealEmptyTiles(row,column){
     let currentTile = [row, column];
