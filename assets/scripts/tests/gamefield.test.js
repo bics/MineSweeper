bfd233d62ea9b3833f5c7267d1fc7450d25a2b5c
bics <bicsi94@gmail.com>
2025-06-05 15:26:34 +0100

    MS-Adding tests

--- assets/scripts/tests/gamefield.test.js
+++ assets/scripts/tests/gamefield.test.js
@@ -26,4 +26,100 @@ describe("Gamefield tests", () =>
             expect(() => new GameField(0, "a", 0)).toThrowError();
         })
     })
+    describe("Playarea initialization tests", () =>
+    {
+        const row = 3;
+        const column = 2;
+        const mines = 1;
+        const gameField = new GameField(row, column, mines);
+        test("Playarea created with correct values", () =>
+        {
+            expect(() => gameField.PlayArea).not.toThrowError();
+            expect(gameField.PlayArea.length).toBe(row);
+            expect(gameField.PlayArea[0].length).toBe(column);
+        })
+        test("Playarea created with correct mine values", () =>
+        {
+            expect(gameField.Mines).toBe(row*column*mines);
+        })
+        test("Playarea hints are correct", () =>
+        {
+            let hintGameField = new GameField(3, 3, 1);
+            hintGameField.PlayArea =
+                [
+                    ["x", "-1", "-1"],
+                    ["-1", "-1", "-1"],
+                    ["-1", "-1", "x"]
+                ];
+            hintGameField.placeHints();
+            expect(hintGameField.PlayArea[0][1]).toBe(1);
+            expect(hintGameField.PlayArea[0][2]).toBe(" ");
+            expect(hintGameField.PlayArea[1][1]).toBe(2);
+            expect(hintGameField.PlayArea[1][0]).toBe(1);
+        })
+    })
+    describe("Gamefield helper method tests", () =>
+    {
+        const row = 3;
+        const column = 2;
+        const mines = 1;
+        const gameField = new GameField(row, column, mines);
+        test("Has flag test, flagged none", () =>
+        {
+            expect(gameField.hasFlags()).toBe(true);
+        })
+        test("Has flag test, flagged 2", () =>
+        {
+            gameField.FlaggedCount = 2;
+            expect(gameField.hasFlags()).toBe(true);
+        })
+        test("Has flag test, flagged all", () =>
+        {
+            gameField.FlaggedCount = 6;
+            expect(gameField.hasFlags()).toBe(false);
+        })
+        test("Return correct amount of neighbours", () =>
+        {
+            const result = gameField.getNeighbourTiles([0, 0]);
+            expect(result.length).toBe(3);
+        })
+        test("Return correct amount of neighbours", () =>
+        {
+            const result = gameField.getNeighbourTiles([1, 1]);
+            expect(result.length).toBe(5);
+        })
+        test("Boundary tests", () =>
+        {
+            expect(gameField.isInBounds([0, 0])).toBe(true);
+            expect(gameField.isInBounds([1, 1])).toBe(true);
+            expect(gameField.isInBounds([2, 1])).toBe(true);
+            expect(gameField.isInBounds([-1, 0])).toBe(false);
+            expect(gameField.isInBounds([6, 0])).toBe(false);
+            expect(gameField.isInBounds([1, 9])).toBe(false);
+        })
+        test("Directional tests, North", () =>
+        {
+            let position = [0, 0];
+            gameField.lookUp(position);
+            expect(position).toStrictEqual([-1, 0]);
+        })
+        test("Directional tests, East", () =>
+        {
+            let position = [0, 0];
+            gameField.lookRight(position);
+            expect(position).toStrictEqual([0, 1]);
+        })
+        test("Directional tests, South", () =>
+        {
+            let position = [0, 0];
+            gameField.lookDown(position);
+            expect(position).toStrictEqual([1, 0]);
+        })
+        test("Directional tests, West", () =>
+        {
+            let position = [0, 0];
+            gameField.lookLeft(position);
+            expect(position).toStrictEqual([0, -1]);
+        })
+    })
 })
